106763b31de4c11f4608005162d9bb8f3e91cf03
Alexandritesu <rovick.nagutoms@gmail.com>
2024-12-08 15:54:56 +0800

    php project

---
+++ components/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="brandy101@gmail.com">brandy101@gmail.com</a>
+      </div>
+
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Novaliches, Quezon City</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">(+63)969141711</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; BRANDY101</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
